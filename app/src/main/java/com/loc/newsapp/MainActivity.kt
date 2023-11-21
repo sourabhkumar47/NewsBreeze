4790ce71c98bd7cc2a5290ab9423f08476caa0af
Sourabh Kumar <20010001030@gateway.edu.in>
2023-11-22 00:13:21 +0530

    onBoardingScreen

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.loc.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
 import com.loc.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsAppTheme {
-
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)){
+                    OnBoardingScreen()
+                }
             }
         }
     }
--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,8 +1,46 @@
 package com.loc.newsapp.presentation.onboarding
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.loc.newsapp.presentation.onboarding.components.OnboardingPage
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OnBoardingScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val pagerState = rememberPagerState(initialPage = 0) {
+            pages.size
+        }
 
+        //as we know different page have different button text,
+        // so we need to change the button text we use derivedStateOf
+        // derivedStateOf is used to change the state of the button text
+        // when the page is changed
+        // we use remember to remember the state of the button text
+
+        val buttonSate = remember {
+            derivedStateOf {
+                when (pagerState.currentPage) {
+                    0 -> listOf("", "Next")
+                    1 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
+                    else -> listOf("", "")
+                }
+            }
+        }
+
+        HorizontalPager(state = pagerState) { index ->
+            OnboardingPage(page = pages[index])
+        }
+
+    }
 }
