c0454f30a4dc36eb00993a0f214398fba8f66e7c
Sourabh Kumar <20010001030@gateway.edu.in>
2023-12-05 03:01:05 +0530

    reversed

--- app/src/main/java/com/loc/newsapp/domain/model/Article.kt
+++ app/src/main/java/com/loc/newsapp/domain/model/Article.kt
@@ -11,9 +11,9 @@ data class Article(
     val author: String?,
     val content: String,
     val description: String,
-    val publishedAt: String?,
-    val source: Source?,
+    val publishedAt: String,
+    val source: Source,
     val title: String,
     @PrimaryKey val url: String,
-    val urlToImage: String?
+    val urlToImage: String
 ) : Parcelable
