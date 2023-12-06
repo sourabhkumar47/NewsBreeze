ad9413c19d84dd5e838e36cffbe2e5b87b787611
Sourabh Kumar <20010001030@gateway.edu.in>
2023-12-07 00:11:23 +0530

    BUG fix: Bookmark screen crash

--- app/src/main/java/com/loc/newsapp/presentation/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/loc/newsapp/presentation/bookmark/BookmarkViewModel.kt
@@ -5,10 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.loc.newsapp.domain.usecases.news.NewsUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
+@HiltViewModel
 class BookmarkViewModel @Inject constructor(
     private val newsUseCases: NewsUseCases
 ) : ViewModel() {
